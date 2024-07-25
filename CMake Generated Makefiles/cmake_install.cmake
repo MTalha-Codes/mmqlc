2e8a2a6355036014a1fd605b789563a0ff677742
Muhammad Talha <khankkhan123apsacs@gmail.com>
2024-07-25 19:43:21 +0500

    Update cmake_install.cmake

--- CMake Generated Makefiles/cmake_install.cmake
+++ CMake Generated Makefiles/cmake_install.cmake
@@ -32,6 +32,7 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.1/bin/mingw/bin/objdump.exe")
 endif()
